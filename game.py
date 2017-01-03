53f493f4975320b1d38a18f978503c35aa2fb77e
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-03 15:41:23 -0800

    Integer and range checker

--- game.py
+++ game.py
@@ -1,12 +1,20 @@
 import random
-
-# Put your code here
 guess_count = 1
 name = raw_input("What is your name?")
-guess = int(raw_input("%s, I'm thinking of a number between 1 and 100. Try to guess my number." % (name)))
+guess = raw_input("%s, I'm thinking of a number between 1 and 100. Try to guess my number." % (name))
+
+while True:
+    try:
+       int(guess)
+       break
+    except ValueError:
+        guess = raw_input("Oops, that's not even a number. Try again.")
+
+while guess > 100 or guess < 1:
+    guess = raw_input("Wow, silly! That is not a number between 1 and 100! Please try again!")
+guess = int(guess)
+
 ranNum = random.randint(1, 100)
-if type(guess) != 'int' or guess > 100 or guess < 100:
-    guess = int(raw_input("Wow, silly, that is not a number between 1 and 100! Please try again."))
 while guess != ranNum:
     if guess > ranNum:
         print("Your guess is too high, try again.")
@@ -18,3 +26,4 @@ while guess != ranNum:
         guess_count += 1
 
 print("Well done %s! You found my number in " % (name) + str(guess_count) + " tries!")
+
